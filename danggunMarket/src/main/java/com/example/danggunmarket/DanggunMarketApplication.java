0eea5fa515cd6ed5a8570c0702b60de9f6d7536a
jang gyu min <100139942+jgm0327@users.noreply.github.com>
2024-09-13 15:03:57 +0900

    Feat: 초기 세팅 (#2)
    개발에 필요한 dependency 추가와 application.yml 수정
    
    Resolves: #1

---
+++ danggunMarket/src/main/java/com/example/danggunmarket/DanggunMarketApplication.java
@@ -0,0 +1,13 @@
+package com.example.danggunmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanggunMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DanggunMarketApplication.class, args);
+    }
+
+}
---
+++ danggunMarket/src/test/java/com/example/danggunmarket/DanggunMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.danggunmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanggunMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
